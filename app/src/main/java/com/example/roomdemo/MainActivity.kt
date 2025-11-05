ed65caac57d338c105ee3895a02b3ee61be11249
therealtommy <sokolovai1887@ya.ru>
2025-11-05 22:38:06 +0700

    Add variables for database

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -9,8 +9,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdemo.ui.theme.RoomDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +38,15 @@ fun ScreenSetup(modifier: Modifier = Modifier) {
     MainScreen(modifier)
 }
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ProductViewModel = viewModel()) {
+    val products by viewModel.allProducts.observeAsState(emptyList())
+    var productName by rememberSaveable { mutableStateOf("") }
+    var quantity by rememberSaveable { mutableStateOf("") }
+    val searchResults by viewModel.searchResults.observeAsState(emptyList())
+    val productsToDisplay = searchResults.ifEmpty { products }
+
+
 }
 
