9447b42d5c4e8a8b8d63a8a1568a829a3b12df6a
therealtommy <sokolovai1887@ya.ru>
2025-11-05 23:14:24 +0700

    add suspend functions for SQL requests

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,8 +17,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +32,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,84 +69,108 @@ fun MainScreen(
     val searchResults by viewModel.searchResults.observeAsState(emptyList())
     val productsToDisplay = searchResults.ifEmpty { products }
 
-    Spacer(modifier = Modifier.height(20.dp))
-
-    Row(
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier.fillMaxWidth()
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
     ) {
-        val buttonColors = buttonColors(containerColor = Color(0xFF6050DC))
-        val buttonModifier = Modifier
-            .size(width = 93.dp, height = 38.dp)
-            .padding(horizontal = 1.dp)
+        OutlinedTextField(
+            value = productName,
+            onValueChange = { productName = it },
+            label = { Text("Product Name") },
+            textStyle = LocalTextStyle.current.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
-            onClick = {
-                viewModel.addProduct(productName, quantity.toIntOrNull() ?: 0)
-                productName = ""
-                quantity = ""
-            },
-            colors = buttonColors,
-            shape = RoundedCornerShape(20.dp),
-            modifier = buttonModifier
-        ) {
-            Text("Add",fontSize = 13.sp)
-        }
-        Button(
-            onClick = {viewModel.findProduct(productName)},
-            colors = buttonColors,
-            shape = RoundedCornerShape(20.dp),
-            modifier = buttonModifier
-        ) {
-            Text("Search",fontSize = 13.sp)
-        }
-        Button(
-            onClick = {viewModel.deleteProduct(productName)},
-            colors = buttonColors,
-            shape = RoundedCornerShape(20.dp),
-            modifier = buttonModifier
-        ) {
-            Text("Delete",fontSize = 13.sp)
-        }
-        Button(
-            onClick = {
-                productName = ""
-                quantity = ""
-            },
-            colors = buttonColors,
-            shape = RoundedCornerShape(20.dp),
-            modifier = buttonModifier
-        ) {
-            Text("Clear",fontSize = 13.sp)
-        }
-    }
+        OutlinedTextField(
+            value = quantity,
+            onValueChange = { quantity = it },
+            label = { Text("Quantity") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            textStyle = LocalTextStyle.current.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
 
-    Spacer(modifier = Modifier.height(20.dp))
-    LazyColumn {
-        item {
-            Row(
-                modifier = Modifier
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            val buttonColors = buttonColors(containerColor = Color.Blue)
+            val buttonModifier = Modifier
+                .size(width = 80.dp, height = 38.dp)
+                .padding(horizontal = 1.dp)
+
+            Button(
+                onClick = {
+                    viewModel.addProduct(productName, quantity.toIntOrNull() ?: 0)
+                    productName = ""
+                    quantity = ""
+                },
+                colors = buttonColors,
+                shape = RoundedCornerShape(15.dp),
+                modifier = buttonModifier
+            ) {
+                Text("+",fontSize = 23.sp,color = Color.White)
+            }
+            Button(
+                onClick = {viewModel.findProduct(productName)},
+                colors = buttonColors,
+                shape = RoundedCornerShape(15.dp),
+                modifier = buttonModifier
+            ) {
+                Text("?",fontSize = 23.sp,color = Color.White)
+            }
+            Button(
+                onClick = {viewModel.deleteProduct(productName)},
+                colors = buttonColors,
+                shape = RoundedCornerShape(15.dp),
+                modifier = buttonModifier
+            ) {
+                Text("-",fontSize = 23.sp,color = Color.White)
+            }
+            Button(
+                onClick = {
+                    productName = ""
+                    quantity = ""
+                },
+                colors = buttonColors,
+                shape = RoundedCornerShape(15.dp),
+                modifier = buttonModifier
+            ) {
+                Text(" ")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        LazyColumn {
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFF6050DC))
+                        .padding(vertical = 4.dp)
+                ) {
+                    Text("ID", Modifier.weight(1f), color = Color.White)
+                    Text("Product", Modifier.weight(3f), color = Color.White)
+                    Text("Quantity", Modifier.weight(2f), color = Color.White)
+                }
+            }
+            items(productsToDisplay) { product ->
+                Row(modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFF6050DC))
                     .padding(vertical = 4.dp)
-            ) {
-                Text("ID", Modifier.weight(1f), color = Color.White)
-                Text("Product", Modifier.weight(3f), color = Color.White)
-                Text("Quantity", Modifier.weight(2f), color = Color.White)
+                ) {
+                    Text(product.id.toString(), Modifier.weight(1f))
+                    Text(product.productName, Modifier.weight(3f))
+                    Text(product.quantity.toString(), Modifier.weight(2f))
+                }
             }
         }
-        items(productsToDisplay) { product ->
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-            ) {
-                Text(product.id.toString(), Modifier.weight(1f))
-                Text(product.productName, Modifier.weight(3f))
-                Text(product.quantity.toString(), Modifier.weight(2f))
-            }
-        }
+
     }
-
 }
-
 
