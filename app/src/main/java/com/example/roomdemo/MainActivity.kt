3bb56c5a4da02b1b295b62dfb36ce6dcabab85b4
therealtommy <sokolovai1887@ya.ru>
2025-11-05 22:47:03 +0700

    Add instruments for management database

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -4,8 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,9 +27,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdemo.ui.theme.RoomDemoTheme
+import com.example.roomdemo.Product
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +63,84 @@ fun MainScreen(
     val searchResults by viewModel.searchResults.observeAsState(emptyList())
     val productsToDisplay = searchResults.ifEmpty { products }
 
+    Spacer(modifier = Modifier.height(20.dp))
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val buttonColors = buttonColors(containerColor = Color(0xFF6050DC))
+        val buttonModifier = Modifier
+            .size(width = 93.dp, height = 38.dp)
+            .padding(horizontal = 1.dp)
+
+        Button(
+            onClick = {
+                viewModel.addProduct(productName, quantity.toIntOrNull() ?: 0)
+                productName = ""
+                quantity = ""
+            },
+            colors = buttonColors,
+            shape = RoundedCornerShape(20.dp),
+            modifier = buttonModifier
+        ) {
+            Text("Add",fontSize = 13.sp)
+        }
+        Button(
+            onClick = {viewModel.findProduct(productName)},
+            colors = buttonColors,
+            shape = RoundedCornerShape(20.dp),
+            modifier = buttonModifier
+        ) {
+            Text("Search",fontSize = 13.sp)
+        }
+        Button(
+            onClick = {viewModel.deleteProduct(productName)},
+            colors = buttonColors,
+            shape = RoundedCornerShape(20.dp),
+            modifier = buttonModifier
+        ) {
+            Text("Delete",fontSize = 13.sp)
+        }
+        Button(
+            onClick = {
+                productName = ""
+                quantity = ""
+            },
+            colors = buttonColors,
+            shape = RoundedCornerShape(20.dp),
+            modifier = buttonModifier
+        ) {
+            Text("Clear",fontSize = 13.sp)
+        }
+    }
+
+    Spacer(modifier = Modifier.height(20.dp))
+    LazyColumn {
+        item {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFF6050DC))
+                    .padding(vertical = 4.dp)
+            ) {
+                Text("ID", Modifier.weight(1f), color = Color.White)
+                Text("Product", Modifier.weight(3f), color = Color.White)
+                Text("Quantity", Modifier.weight(2f), color = Color.White)
+            }
+        }
+        items(productsToDisplay) { product ->
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+            ) {
+                Text(product.id.toString(), Modifier.weight(1f))
+                Text(product.productName, Modifier.weight(3f))
+                Text(product.quantity.toString(), Modifier.weight(2f))
+            }
+        }
+    }
 
 }
+
 
